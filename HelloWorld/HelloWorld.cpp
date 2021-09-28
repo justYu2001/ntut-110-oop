a9dc70a3a7e0ebd18d48bc5c116ec67eef86b0d8
justYu2001 <jack4781012@gmail.com>
2021-09-28 21:32:58 +0800

    hw0

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "HelloWorld.h"
+using namespace std;
+
+int main(){
+    cout << PrintHelloWorld();
+    return 0;
+}
---
+++ HelloWorld/HelloWorld.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+string PrintHelloWorld(){
+    return "Hello World! \n";
+}
