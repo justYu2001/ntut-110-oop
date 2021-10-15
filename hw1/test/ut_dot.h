01c6a9690a3ee70ac12939c5de5dc03a942b6b1a
justYu2001 <jack4781012@gmail.com>
2021-10-15 11:02:24 +0800

    add new test cases in hw1

--- hw1/test/ut_dot.h
+++ hw1/test/ut_dot.h
@@ -11,6 +11,15 @@ TEST(Dot, TwoDimDotTwoDim) {
     ASSERT_NEAR(18.0, result, 0.01);
 }
 
+TEST(Dot, TwoDimDotTwoDim2) {
+    double u[2] = {5, 8};
+    int dim_u = 2;
+    double v[2] = {5, 5};
+    int dim_v = 2;
+    double result = dot(u, dim_u, v, dim_v);
+    ASSERT_NEAR(65.0, result, 0.01);
+}
+
 TEST(Dot, ThreeDimDotThreeDim) {
     double u[3] = {1, 3, -5};
     int dim_u = 3;
@@ -18,6 +27,15 @@ TEST(Dot, ThreeDimDotThreeDim) {
     int dim_v = 3;
     double result = dot(u, dim_u, v, dim_v);
     ASSERT_NEAR(3.0, result, 0.01);
+}
+
+TEST(Dot, ThreeDimDotThreeDim2) {
+    double u[3] = {4, 5, 6};
+    int dim_u = 3;
+    double v[3] = {1, 2, 3};
+    int dim_v = 3;
+    double result = dot(u, dim_u, v, dim_v);
+    ASSERT_NEAR(32.0, result, 0.01);
 }
 
 TEST(Dot, ThreeDimDotFourDim) {
@@ -28,7 +46,23 @@ TEST(Dot, ThreeDimDotFourDim) {
     ASSERT_THROW(dot(u, dim_u, v, dim_v), std::string);
 }
 
+TEST(Dot, ThreeDimDotFourDim2) {
+    double u[3] = {5, 8, 9};
+    int dim_u = 3;
+    double v[4] = {1, 2, 3, 4};
+    int dim_v = 4;
+    ASSERT_THROW(dot(u, dim_u, v, dim_v), std::string);
+}
+
 TEST(Dot, DotException) {
+    double u[3] = {4, 0, 3};
+    int dim_u = -1;
+    double v[4] = {5, 8, 1, 2};
+    int dim_v = 4;
+    ASSERT_THROW(dot(u, dim_u, v, dim_v), std::string);
+}
+
+TEST(Dot, DotException2) {
     double u[3] = {2, 3, 1};
     int dim_u = -1;
     double v[4] = {3, 4, 5, 6};
