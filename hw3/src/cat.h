9222afa6dd85a8eae4b0a90e8b0227e384eee721
justYu2001 <jack4781012@gmail.com>
2021-11-03 16:20:39 +0800

    add hw3

---
+++ hw3/src/cat.h
@@ -0,0 +1,57 @@
+#include <string>
+
+using std::string;
+
+class Cat {
+public:
+
+    Cat(string name, double weight) {
+        _name = name;
+        _weight = weight;
+        _toyAmount = 0;
+        _capacityOfToyCollections = 2;
+        _toyCollections = new string[_capacityOfToyCollections];
+    }
+
+    ~Cat() {
+        delete [] _toyCollections;
+    }
+
+    string getName() {
+        return _name;
+    }
+
+    double getWeight() {
+        return _weight;
+    }
+
+    void addNewToy(string toyName) {
+        if(_toyAmount == _capacityOfToyCollections) {
+            reserveToyCollections();
+        }
+        _toyCollections[_toyAmount++] = toyName;
+    }
+
+    string getToy(int index) {
+        return _toyCollections[index];
+    }
+
+private:
+    string _name;
+    double _weight;
+    string * _toyCollections;
+    int _toyAmount;
+    int _capacityOfToyCollections;
+
+    void reserveToyCollections() {
+        int newCapacity = _toyAmount * 2;
+        string *newToyCollections = new string[newCapacity];
+        for(int i = 0; i < _toyAmount; i++) {
+            newToyCollections[i] = _toyCollections[i];
+            _toyCollections[i].clear();
+        }
+        _capacityOfToyCollections = newCapacity;
+        delete [] _toyCollections;
+        _toyCollections = newToyCollections;
+    }
+};
---
+++ hw3/test/ut_cat.h
@@ -0,0 +1,78 @@
+#include "../src/cat.h"
+
+#include <string>
+
+using std::string;
+
+// add your test here
+
+TEST(Cat, CatGetName1) {
+    string name = "Kitty";
+    double weight = 3.0;
+    Cat cat(name, weight);
+    string result = cat.getName();
+    EXPECT_TRUE(name == result);
+}
+
+TEST(Cat, CatGetName2) {
+    string name = "Lilly";
+    double weight = 2.8;
+    Cat cat(name, weight);
+    string result = cat.getName();
+    EXPECT_TRUE(name == result);
+}
+
+TEST(Cat, CatGetWeight1) {
+    string name = "Kitty";
+    double weight = 3.0;
+    Cat cat(name, weight);
+    double result = cat.getWeight();
+    ASSERT_NEAR(3.0, result, 0.001);
+}
+
+TEST(Cat, CatGetWeight2) {
+    string name = "Lilly";
+    double weight = 2.8;
+    Cat cat(name, weight);
+    double result = cat.getWeight();
+    ASSERT_NEAR(2.8, result, 0.001);
+}
+
+TEST(Cat, CatAddNewToy1) {
+    string name = "Kitty";
+    double weight = 3.0;
+    Cat cat(name, weight);
+    cat.addNewToy("ball");
+    EXPECT_TRUE("ball" == cat.getToy(0));
+}
+
+TEST(Cat, CatAddNewToy2) {
+    string name = "Lilly";
+    double weight = 2.8;
+    Cat cat(name, weight);
+    cat.addNewToy("cat teaser wand");
+    EXPECT_TRUE("cat teaser wand" == cat.getToy(0));
+}
+
+TEST(Cat, CatGetToy1) {
+    string name = "Kitty";
+    double weight = 3.0;
+    Cat cat(name, weight);
+    cat.addNewToy("ball");
+    cat.addNewToy("cat teaser wand");
+    EXPECT_TRUE("ball" == cat.getToy(0));
+    EXPECT_TRUE("cat teaser wand" == cat.getToy(1));
+}
+
+TEST(Cat, CatGetToy2) {
+    string name = "Lilly";
+    double weight = 2.8;
+    Cat cat(name, weight);
+    cat.addNewToy("cat teaser wand");
+    cat.addNewToy("catnip mice");
+    cat.addNewToy("ball");
+    EXPECT_TRUE("cat teaser wand" == cat.getToy(0));
+    EXPECT_TRUE("catnip mice" == cat.getToy(1));
+    EXPECT_TRUE("catnip mice" == cat.getToy(1));
+    EXPECT_TRUE("ball" == cat.getToy(2));
+}
