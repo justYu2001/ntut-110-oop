6ebae4cbcfac5158086e46599e47996abca69f9d
justYu2001 <jack4781012@gmail.com>
2021-11-26 10:24:08 +0800

    add midterm exam

---
+++ midterm-exam/src/binary.h
@@ -0,0 +1,106 @@
+#ifndef Binary_H
+#define Binary_H
+
+#include <string>
+#include <bitset>
+
+using std::string;
+using std::to_string;
+using std::bitset;
+
+class Binary{
+private:
+    int _size;
+    string _valueString;
+    bitset<100> _bitValue;
+
+public:
+    Binary(){ }
+
+    Binary(int input[],int s){
+        _size = s;
+        for(int i = 0; i < _size; i++) {
+            int currentBit = input[i];
+            if(currentBit != 0 && currentBit != 1) {
+                throw string("error");
+            }
+            _valueString += to_string(currentBit);
+        }
+
+        _bitValue = bitset<100>(_valueString);
+
+    }
+    Binary(const Binary & b){
+        _size = b._size;
+        _bitValue = b._bitValue;
+        _valueString = b._valueString;
+    }
+
+    ~Binary(){ }
+
+    Binary & operator=(Binary const &b){
+        _size = b._size;
+        _bitValue = b._bitValue;
+        _valueString = b._valueString;
+        return *this;
+    }
+
+    Binary operator+(Binary const &b){
+        int result = this->toInt() + b.toInt();
+        this->_bitValue = bitset<100>(result);
+        return *this;
+    }
+
+    Binary operator-(Binary const &b){
+        int result = this->toInt() - b.toInt();
+        this->_bitValue = bitset<100>(result);
+        return *this;
+    }
+
+    Binary operator>>(int a){ 
+        _bitValue >>= a;
+        _valueString =  _bitValue.to_string().substr(100 - _size, _size);
+        return *this;
+    }
+
+    Binary operator<<(int a){
+        _bitValue <<= a;
+        _valueString =  _bitValue.to_string().substr(100 - _size, _size);
+        return *this;
+    }
+
+    void resize(int new_size){
+        if(new_size < _size) {
+            throw string("Error");
+        }
+
+        for(int i = _size; i < new_size; i++) {
+            _valueString = "0" + _valueString;
+        }
+        _bitValue = bitset<100>(_valueString);
+        _size = new_size;
+    }
+    
+    void setBit(int pos,int a){
+        if(a != 0 || a != 1) {
+            throw string("error");
+        }
+
+        if(pos >= _size) {
+            throw string("error");
+        }
+
+        _valueString[pos] = a + '0';
+        _bitValue = bitset<100>(_valueString);
+    }
+
+    int toInt() const{
+        return (int)_bitValue.to_ullong();
+    }
+
+    string toString() const{
+        return _valueString;
+    }
+};
+
+#endif
