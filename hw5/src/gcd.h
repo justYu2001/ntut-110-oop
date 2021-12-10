a86db9fe681feaff9b616afa77fa7be7500c811c
justYu2001 <t109590011@ntut.edu.tw>
2021-12-10 11:04:41 +0800

    fix gcd negative bug in hw5

--- hw5/src/gcd.h
+++ hw5/src/gcd.h
@@ -1,5 +1,15 @@
+#ifndef GCD_H
+#define GCD_H
+
+int abs(int number) {
+      return number >= 0 ? number : -number;
+}
 // return the (positive) greatest common divisor of two integers
 // a, b may be positive or negative
 int gcd(int a, int b) {
+    a = abs(a);
+    b = abs(b);
     return a % b != 0 ? gcd(b, a % b) : b;
 }
+
+#endif
